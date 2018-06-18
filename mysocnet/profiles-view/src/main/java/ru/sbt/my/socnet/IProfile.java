cd4e7bd1e4382823a2208c0853e0fc6f0592166f
Pavel Shulepov <pshulepov@gmail.com>
2018-06-18 22:04:41 +0500

    Описал методы IProfile

--- mysocnet/profiles-view/src/main/java/ru/sbt/my/socnet/IProfile.java
+++ mysocnet/profiles-view/src/main/java/ru/sbt/my/socnet/IProfile.java
@@ -1,4 +1,42 @@
 package ru.sbt.my.socnet;
 
+/*
+    IProfile - интерфейс "Профиль" - профиль пользователя mysocnet
+
+ */
+
+import java.util.ArrayList;
+
 public interface IProfile {
+
+    // Метод возращает Имя пользователя
+    public String getProfileName();
+
+    // Метод возвращает Id профиля - уникальное поле
+    public int getProfileId();
+
+    // Метод возвращает возраст пользователя
+    public int getProfileAge();
+
+    // Метод возвращает true - если пользователь онлайн,
+    public boolean isOnline();
+
+    // Метод возвращает true если profileId - друг, иначе false
+    public boolean isFriend(int profileId);
+
+    // Метод добавляет профиль profileId в друзья
+    public void addFriend(int profileId);
+
+    // Метод удаляет профиль profileId из друзей
+    public void delFriend(int profileId);
+
+    // Метод полсылает сообщение message от profileId
+    // Возвращает messageId
+    public int sendMessage(int profileId, String message);
+
+    // Метод удаляет сообщение messageId
+    public void delMessage(int profileId, int messageId);
+
+    public ArrayList<String> getProfile();
+
 }
