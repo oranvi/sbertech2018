d8b438798361cadb158dacb3899004b1723b648c
oranvi <a_or@inbox.ru>
2018-06-19 15:56:14 +0500

    определено содержание модуля wall

--- mysocnet/wall/src/main/java/ru/sbt/my/socnet/IWall.java
+++ mysocnet/wall/src/main/java/ru/sbt/my/socnet/IWall.java
@@ -1,4 +1,19 @@
 package ru.sbt.my.socnet;
 
-public interface IWall { // 
+public interface IWall {
+
+    void addWallMessage(String my_id, String friend_id, String gift_id, String text_message); // * на своей стене или стене друга
+
+    // * добавить в сообщение на стене фото
+
+    // добавить ссылку на сообщение из сообщества
+
+    // отображение времени/даты сообщения
+
+    // создание комментария к сообщению на стене
+
+    // лайк
+
+    // отображение количества просмотров
+
 }
