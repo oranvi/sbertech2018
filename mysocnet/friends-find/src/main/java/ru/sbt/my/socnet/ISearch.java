6758a7a3af970316f96b1df244154c781543186f
oranvi <a_or@inbox.ru>
2018-06-19 11:16:26 +0500

    определено содержание 2х модулей: поиск/добавление друзей и подарки

---
+++ mysocnet/friends-find/src/main/java/ru/sbt/my/socnet/ISearch.java
@@ -0,0 +1,23 @@
+package ru.sbt.my.socnet;
+
+// * - помечены связи с другими модулями
+
+public interface ISearch { // интерфейс для поиска/добавления друзей
+
+    void searchFriend(String name_surname); // поиск друга
+
+    void searchFriendAccurately(String name_surname, String country, String town, int ageFrom, int ageTo); // точный поиск друга
+
+    void offerFriendship(String user_id); // предложение дружбы из результатов поиска
+
+    // * рекомендации при поиске
+
+    // * перейти на страницу пользователя из результатов поиска
+
+    // * отправить предложение дружбы со страницы пользователя
+
+    // * отображение фото пользователя в результатах поиска
+
+    // * (?) удаление друга
+
+}
---
+++ mysocnet/gifts/src/main/java/ru/sbt/my/socnet/IGift.java
@@ -0,0 +1,15 @@
+package ru.sbt.my.socnet;
+
+// * - помечены связи с другими модулями
+
+public interface IGift { // интерфейс для отправления подарков
+
+    void choiceGift(String my_id);// выбор подарка
+
+    void makeGift(String gift_id, String friend_id, String text_gift); // * содержит choiceGift()
+
+    void makeGiftConfidentially(String gift_id, String friend_id, String text_gift); // * содержит choiceGift()
+
+    // * отображение подарков на странице пользователя (получателя)
+
+}
